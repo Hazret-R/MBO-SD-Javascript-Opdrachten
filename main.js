e5ba3759eac4adec7660921a16937661b5672f05
Hazret-R <302173137@student.rocmondriaan.nl>
2024-09-10 09:20:37 +0200

    Javascript opdrachten

---
+++ main.js
@@ -0,0 +1,17 @@
+// Opdracht variabelen
+
+const userName = "Hazret";
+let currentTemp = 20;
+
+console.log(userName);
+alert(`Hallo ${userName}, het is nu ${currentTemp} graden`);
+
+// Opdracht interactie
+
+const askedName = prompt("Wat is je naam?");
+alert(askedName)
+const cookiesAccepted = confirm("Accepteer je dat we je cookies mogen bewaren?")
+console.log(cookiesAccepted)
+
+const myCity = 'Den Haag';
+alert('Ik woon in ' + myCity);
