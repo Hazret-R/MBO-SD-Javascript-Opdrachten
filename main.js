91e3e84ad91e26adcfad99b1ea73c97469decf8c
Hazret-R <302173137@student.rocmondriaan.nl>
2024-09-10 09:33:28 +0200

    ;

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ console.log(userName);
 alert(`Hallo ${userName}, het is nu ${currentTemp} graden`);
 
 currentTemp += 5;
-console.log(currentTemp)
+console.log(currentTemp);
 
 // Opdracht interactie, Hoofdstuk 2.2
 
 const askedName = prompt("Wat is je naam?");
-alert(askedName)
-const cookiesAccepted = confirm("Accepteer je dat we je cookies mogen bewaren?")
-console.log(cookiesAccepted)
+alert(askedName);
+const cookiesAccepted = confirm("Accepteer je dat we je cookies mogen bewaren?");
+console.log(cookiesAccepted);
 
 const myCity = 'Den Haag';
 alert('Ik woon in ' + myCity);
